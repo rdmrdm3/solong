81692450cd356d5d18e47be6b7224faf39f87ff0
Raphael Di Marzio <rdi-marz@c2r4p6.42nice.fr>
2022-03-29 15:20:00 +0200

    still not working

--- srcs/ft_presskey.c
+++ srcs/ft_presskey.c
@@ -6,16 +6,16 @@
 /*   By: rdi-marz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 15:53:14 by rdi-marz          #+#    #+#             */
-/*   Updated: 2022/03/28 23:06:40 by rdi-marz         ###   ########.fr       */
+/*   Updated: 2022/03/29 15:18:19 by rdi-marz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_so_long.h"
 
-int	ft_presskey(int keycode, t_map **map)
+int	ft_presskey(int keycode, void *m)
 {
-//	t_map	**map;
-//	map = (t_map **)m;
+	t_map	*map;
+	map = (t_map *) m;
 //	int y = (*map)->pspot[0][0];
 //   	int x = (*map)->pspot[0][1];
 //	printf("press increm=%i\n", (*map)->increm);
@@ -25,40 +25,37 @@ int	ft_presskey(int keycode, t_map **map)
 //	printf("(y, x-1)=>%c\n", (*map)->maze[y][x - 1]);	
 //	printf("(y+1, x)=>%c\n", (*map)->maze[y + 1][x]);	
 //	printf("(y-1, x+1)=>%c\n", (*map)->maze[y - 1][x]);	
-	printf("anim =%i\n", (*map)->anim);
-	if ((*map)->anim == 0)
+//	printf("anim =%i\n", (*map)->anim);
+	if (map->anim == 0)
 	{
 		if (keycode == 53)
+		{
 			printf("you just pressed escape :)\n");
+			exit(1);
+		}
 		else if (keycode == 13)
 		{
 			printf("you just pressed w\n");
-			(*map)->mv = 1;
-			(*map)->anim = 1;
-//			if ((*map)->maze[(*map)->pspot[0][1] - 1][(*map)->pspot[0][0]] != '1')
-//				ft_pacmove(1, (*map)->pspot[0][0], (*map)->pspot[0][1], map);
+			map->mv = 1;
+			map->anim = 1;
 		}
 		else if (keycode == 0)
 		{
 			printf("you just pressed a\n");
-			(*map)->mv = 4;
-			(*map)->anim = 1;
-//			if ((*map)->maze[(*map)->pspot[0][1]][(*map)->pspot[0][0] - 1] != '1')
-//				ft_pacmove(4, (*map)->pspot[0][0], (*map)->pspot[0][1], map);
+			map->mv = 4;
+			map->anim = 1;
 		}
 		else if (keycode == 1)
 		{
 			printf("you just pressed s\n");
-			(*map)->mv = 3;
-			(*map)->anim = 1;
-//			if ((*map)->maze[(*map)->pspot[0][1] + 1][(*map)->pspot[0][0]] != '1')
-//				ft_pacmove(3, (*map)->pspot[0][0], (*map)->pspot[0][1], map);
+			map->mv = 3;
+			map->anim = 1;
 		}
 		else if (keycode == 2)
 		{
 			printf("you just pressed d\n");
-			(*map)->mv = 2;
-			(*map)->anim = 1;
+			map->mv = 2;
+			map->anim = 1;
 //		if ((*map)->maze[y][x + 1] != '1')
 //		{
 //			ft_pacmove(2, (*map)->pspot[0][0], (*map)->pspot[0][1], map);
--- srcs/ft_so_long.c
+++ srcs/ft_so_long.c
@@ -6,7 +6,7 @@
 /*   By: rdi-marz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 22:38:30 by rdi-marz          #+#    #+#             */
-/*   Updated: 2022/03/29 14:12:55 by rdi-marz         ###   ########.fr       */
+/*   Updated: 2022/03/29 14:58:34 by rdi-marz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,24 @@ int	main(int argc, char **argv)
 {
 	int		parsingresult;
 	t_map	*map;
+	t_map	m;
 
-	map = malloc (1 * sizeof(t_map));
+	map = &m;
+//	map = malloc (1 * sizeof(t_map));
 	parsingresult = ft_parsing(argc, argv, &map);
 	if (parsingresult != 1)
 		return (ft_exitfail(1, parsingresult));
 	ft_displaymap(&map);
-	printf("anim=%i, mv=%i\n", map->anim, map->mv);
-	printf("after displaymap\n");
+//	printf("anim=%i, mv=%i, pacmouth=%i\n", map->anim, map->mv, map->pacmouth);
+	map->pacmouth = 0;
+	map->anim = 0;
+	map->mv = 0;
+//	printf("after displaymap\n");
 	mlx_hook(map->win, 17, 0L, ft_closegame, map);
-	mlx_hook(map->win, 2, 1L<<0, ft_presskey, map);
-//		mlx_key_hook((*map)->win, ft_presskey, map);
-	mlx_loop_hook(map->win, ft_presskey, map);
-	printf("after ft_game\n");
+//	mlx_hook(map->win, 2, 1L<<0, ft_presskey, map);
+	mlx_key_hook(map->win, ft_presskey, map);
+	mlx_loop_hook(map->mlx, ft_presskey, map);
+//	printf("after ft_game\n");
 	mlx_loop(map->mlx);
 	return (0);
 }
